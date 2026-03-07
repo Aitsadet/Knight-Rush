9aa271ce809f619af0d90960fe9a3fb57ebbd321
KengPeerapol <peerapol.poth@bumail.net>
2026-03-08 06:20:19 +0700

    Level Design (2 Stage)

--- Assets/Script/Demo_GM.cs
+++ Assets/Script/Demo_GM.cs
@@ -1,178 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-
-
-
-public class Demo_GM : MonoBehaviour {
-
-
-
+public class Demo_GM : MonoBehaviour
+{
     public static Demo_GM Gm;
 
+    [Header("UI Settings")]
+    [Tooltip("ต้องใส่ Image ทั้งหมด 8 ช่อง (0-7)")]
     public Image[] UIImage;
 
-    // Use this for initialization
-    void Awake () {
-        Screen.fullScreen = false;
+    // กำหนดสีไว้ตรงนี้จะได้แก้ที่เดียวจบ
+    private Color32 normalColor = new Color32(255, 255, 255, 255); // สีปกติ (ขาว)
+    private Color32 pressedColor = new Color32(180, 180, 180, 255); // สีตอนกด (เทา)
 
+    void Awake()
+    {
+        Screen.fullScreen = false;
         Gm = this;
     }
-	
-	// Update is called once per frame
-	void Update () {
 
+    void Update()
+    {
         KeyUPDownchange();
-
-
     }
 
-
-    void InitColor()
+    // ฟังก์ชันช่วยเปลี่ยนสีแบบปลอดภัย เช็กก่อนว่ามีของใน Array ไหม
+    void SetKeyColor(int index, bool isPressed)
     {
-
-        for (int i = 0; i < UIImage.Length; i++)
+        // เช็กว่า Index นี้มีอยู่ใน Array และมีการลาก Object มาใส่จริงๆ
+        if (UIImage != null && index < UIImage.Length && UIImage[index] != null)
         {
-            UIImage[i].color = new Color(255, 255, 255);
-
-
+            UIImage[index].color = isPressed ? pressedColor : normalColor;
         }
-
     }
 
     public void KeyUPDownchange()
     {
-        // wsad
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            Color myColor = new Color32(255, 255, 255, 255);
+        // --- WSAD ---
+        if (Input.GetKeyDown(KeyCode.W)) SetKeyColor(0, true);
+        if (Input.GetKeyUp(KeyCode.W)) SetKeyColor(0, false);
 
-            Demo_GM.Gm.UIImage[2].color = myColor;
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            Color myColor = new Color32(255, 255, 255, 255);
+        if (Input.GetKeyDown(KeyCode.S)) SetKeyColor(1, true);
+        if (Input.GetKeyUp(KeyCode.S)) SetKeyColor(1, false);
 
-            Demo_GM.Gm.UIImage[3].color = myColor;
-        }
+        if (Input.GetKeyDown(KeyCode.A)) SetKeyColor(2, true);
+        if (Input.GetKeyUp(KeyCode.A)) SetKeyColor(2, false);
 
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            Color myColor = new Color32(255, 255, 255, 255);
+        if (Input.GetKeyDown(KeyCode.D)) SetKeyColor(3, true);
+        if (Input.GetKeyUp(KeyCode.D)) SetKeyColor(3, false);
 
-            Demo_GM.Gm.UIImage[0].color = myColor;
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            Color myColor = new Color32(255, 255, 255, 255);
+        // --- Mouse ---
+        if (Input.GetKeyDown(KeyCode.Mouse0)) SetKeyColor(4, true);
+        if (Input.GetKeyUp(KeyCode.Mouse0)) SetKeyColor(4, false);
 
-            Demo_GM.Gm.UIImage[1].color = myColor;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Color myColor = new Color32(180, 180, 180, 255);
+        if (Input.GetKeyDown(KeyCode.Mouse1)) SetKeyColor(5, true);
+        if (Input.GetKeyUp(KeyCode.Mouse1)) SetKeyColor(5, false);
 
-            Demo_GM.Gm.UIImage[2].color = myColor;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Color myColor = new Color32(180, 180, 180, 255);
+        // --- Other Keys ---
+        if (Input.GetKeyDown(KeyCode.Alpha1)) SetKeyColor(6, true);
+        if (Input.GetKeyUp(KeyCode.Alpha1)) SetKeyColor(6, false);
 
-            Demo_GM.Gm.UIImage[3].color = myColor;
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Color myColor = new Color32(180, 180, 180, 255);
-
-            Demo_GM.Gm.UIImage[0].color = myColor;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Color myColor = new Color32(180, 180, 180, 255);
-
-            Demo_GM.Gm.UIImage[1].color = myColor;
-        }
-
-        ///
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            Color myColor = new Color32(255, 255, 255, 255);
-
-            Demo_GM.Gm.UIImage[4].color = myColor;
-        }
-        if (Input.GetKeyUp(KeyCode.Mouse1))
-        {
-            Color myColor = new Color32(255, 255, 255, 255);
-
-            Demo_GM.Gm.UIImage[5].color = myColor;
-        }
-
-   
-
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-
-            Color myColor = new Color32(180, 180, 180, 255);
-
-            Demo_GM.Gm.UIImage[4].color = myColor;
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse1))
-        {
-
-
-
-            Color myColor = new Color32(180, 180, 180, 255);
-
-            Demo_GM.Gm.UIImage[5].color = myColor;
-
-        }
-
-    
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-
-            Color myColor = new Color32(180, 180, 180, 255);
-
-            Demo_GM.Gm.UIImage[6].color = myColor;
-        }
-
-
-    
-        if (Input.GetKeyUp(KeyCode.Alpha1))
-        {
-
-            Color myColor = new Color32(255, 255, 255, 255);
-
-            Demo_GM.Gm.UIImage[6].color = myColor;
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
-            Color myColor = new Color32(180, 180, 180, 255);
-
-            Demo_GM.Gm.UIImage[7].color = myColor;
-        }
-
-
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-
-            Color myColor = new Color32(255, 255, 255, 255);
-
-            Demo_GM.Gm.UIImage[7].color = myColor;
-        }
-
-
+        if (Input.GetKeyDown(KeyCode.Space)) SetKeyColor(7, true);
+        if (Input.GetKeyUp(KeyCode.Space)) SetKeyColor(7, false);
     }
 
+    // ฟังก์ชันรีเซ็ตสีทั้งหมด (ถ้าต้องการเรียกใช้)
+    void InitColor()
+    {
+        if (UIImage == null) return;
+        for (int i = 0; i < UIImage.Length; i++)
+        {
+            if (UIImage[i] != null) UIImage[i].color = normalColor;
+        }
+    }
 }
